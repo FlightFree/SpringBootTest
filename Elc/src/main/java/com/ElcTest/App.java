22bc73ca49b9ca9a9ee7edc9a35b12d5cf270e21
FlightFree <1132165510@qq.com>
2019-06-19 22:16:26 +0800

    启动类调整

--- Elc/src/main/java/com/ElcTest/App.java
+++ Elc/src/main/java/com/ElcTest/App.java
@@ -20,23 +20,8 @@ import java.time.LocalDate;
 @MapperScan("com.ElcTest.dao")
 public class App 
 {
-    @Autowired
-    private UserMapper userMapper;
-
-    @RequestMapping("/")
-    public String home(){
-        User user= userMapper.selectByPrimaryKey(1);
-        if(user==null){
-            return "用户对象不存在";
-        }else{
-            return user.getName();
-        }
-
-    }
     public static void main( String[] args )
     {
-        LocalDate t=LocalDate.now();
-        System.out.println( t );
         SpringApplication.run(App.class,args);
     }
 
